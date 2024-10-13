bcd2de2f80eaf0ba95bb20a447babb0cbaae0963
Vanja <vanjadjuric32@gmail.com>
2024-10-13 16:23:38 +0200

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,5 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jpn-travel';
+  title = 'jpn-trvl';
 }
